b5f79eb43965cfc103d78b898cf8e65040a7267e
priyeshkh <khatiwadapriyesh@gmail.com>
2024-06-20 17:34:51 +0545

    vue starter project files for beginners

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>Hello World!</h1>
+    <button>Add Tak</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style></style>
